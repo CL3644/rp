bff35b02bbefea5a281e1879d2e2490f36823a2e
Calvin Lu <cl3644@columbia.edu>
2021-02-06 04:44:18 +0000

    might be done with part 1, not sure

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,15 @@
 class Movie < ActiveRecord::Base
+# 	attr_accessor :all_ratings
+	
+# 	:all_ratings = ['G', 'PG', 'PG-13', 'R']
+	
+	def self.all_ratings
+		['G', 'PG', 'PG-13', 'R']
+	end
+
+	def self.with_ratings(ratings_list)
+		return Movie.where(rating: ratings_list)
+	end
+	
 end
+
